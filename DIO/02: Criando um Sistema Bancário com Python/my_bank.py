09df5fef30b94684c8f855f8951485be6c23debc
Alberto Leal <alberto.sanctum@gmail.com>
2025-08-20 14:04:35 -0300

    Atualizando opção de deposito

--- DIO/02: Criando um Sistema Bancário com Python/my_bank.py
+++ DIO/02: Criando um Sistema Bancário com Python/my_bank.py
@@ -20,14 +20,23 @@ limite_saques = 3
 while True:
   opcao = input(menu)
   
-  if opcao == 1:
-    print ("depósito")
-  
-  elif opcao == 2:
-    print ("saque")
-  
-  elif opcao == 3:
-    print ("extrato")
-  
-  elif opcao == 0:
+  if opcao == "1":
+    deposito = (float(input('Digite o valor a depositar: ')))
+    if deposito > 0:
+      saldo += deposito
+      extrato += f'Depositado: R${deposito:.2f}̣\n'
+
+
+  elif opcao == "2":
+    print("Depois eu monto essa parte..")
+  # elif opcao == "3":
+  #   print ("extrato")
+    
+  #   for input in deposito:
+  #     print (f'Deposito de R$ {deposito}')
+    
+  elif opcao == "0":
     break
+
+else:
+  print ("Opcao invalida!")
